07d83cf6faf9bdaa80dbaee13843658bd84e847a
goreleaserbot <joulman+github@gmail.com>
2022-01-26 23:31:48 -0500

    Brew formula update for terraform-credentials-vault version v1.0.6

--- Formula/terraform-credentials-vault.rb
+++ Formula/terraform-credentials-vault.rb
@@ -8,17 +8,17 @@ class TerraformCredentialsVault < Formula
   version "1.0.6"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/oulman/terraform-credentials-vault/releases/download/v1.0.6/terraform-credentials-vault_1.0.6_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "08fffa45ac6b919f17dcdbb0f14d1b1ffb49fc805ebbd5a647e4cf29193a80fd"
+    if Hardware::CPU.arm?
+      url "https://github.com/oulman/terraform-credentials-vault/releases/download/v1.0.6/terraform-credentials-vault_1.0.6_darwin_arm64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "20283f3052366c3973f28b49cb900676f3e55ee318a154062770f874b3cab0d2"
 
       def install
         bin.install "terraform-credentials-vault"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/oulman/terraform-credentials-vault/releases/download/v1.0.6/terraform-credentials-vault_1.0.6_darwin_arm64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "3c36a86a71ff712a8b18192075305fb017dc953ae563794c58d17139307f6339"
+    if Hardware::CPU.intel?
+      url "https://github.com/oulman/terraform-credentials-vault/releases/download/v1.0.6/terraform-credentials-vault_1.0.6_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "3947487f436fc582abc9fd65b1d6b97278141c0fce162c9e6b3b6911c9ff458e"
 
       def install
         bin.install "terraform-credentials-vault"
@@ -27,17 +27,17 @@ class TerraformCredentialsVault < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/oulman/terraform-credentials-vault/releases/download/v1.0.6/terraform-credentials-vault_1.0.6_linux_amd64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "a39195e959394047f8a412b62118156cb2ce9b84ce69a1207d2ac5c92e28c28c"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/oulman/terraform-credentials-vault/releases/download/v1.0.6/terraform-credentials-vault_1.0.6_linux_arm64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "2ab257c86cb89cfe867983c465e38eb657c7eaa75b5333d2b44e31536a1deb34"
 
       def install
         bin.install "terraform-credentials-vault"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/oulman/terraform-credentials-vault/releases/download/v1.0.6/terraform-credentials-vault_1.0.6_linux_arm64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "e7530c7bc82819b53fa36433fc64cc5c41438ce3ca75acc39bb8c5b95e1c0b47"
+    if Hardware::CPU.intel?
+      url "https://github.com/oulman/terraform-credentials-vault/releases/download/v1.0.6/terraform-credentials-vault_1.0.6_linux_amd64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "bf62594c79c2b94c611ae7b08301f414e8cf3751cf798ff0661cc6a77a26ecd1"
 
       def install
         bin.install "terraform-credentials-vault"
